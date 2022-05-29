4ab6c0265737f00f025158051cba40cce1a6ebe7
Ansus-code <85507987+Ansus-code@users.noreply.github.com>
2022-05-29 19:09:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+name : "Monica", 
+gender : "female",
+age : 17,
+email : "monica@dingdong.com"
+};
+const secondUser = {
+name : "Wendy",
+gender : "Male",
+age : 23,
+emai : "wendy@dingdong.com"
+};
+
+conts fovoriteColor = array ["Yellow", "White", "Purple", "Pink"];
+const favoriteRestoran = array ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]
+cont educationSet = new Set();
+
+let educationSd ={
+nama : "SD Negeri 01";
+city : Jakarta;
+gradution : 2006;
+}
+
+let educationSMP {
+ nama : "SMP 02";
+ city : "Jakarta";
+ gradution : 2019;
+};
+ 
+ let educationSMA {
+ nama : "SMA 03 "
+ city : "Tangerang"
+ };
+let map =new Map()
+
+map.set =("nama", "Ansus");
+map.set =("age", 17);
+map.set =(educationSMA, "SMA 03")
+map.set =(favoriteColor, "white")
+map.set =(favoriteRestoran, "Sushi");
+
+console.log(fisrtUser);
+console.log(eduacationSet);
+console.log(favoriteColor);
+console.log(favoriteRestoran);
+
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
